e68a84ce46e3f3551dfe197b8d21c7def99bd5a9
mariaach <cardenashenaomaria@gmail.com>
2023-04-21 14:24:25 -0500

    proyecto real

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from "./componentes/Header/Header"
+import Formulario from './componentes/Formulario/Formulario';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
