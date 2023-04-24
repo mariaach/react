ec7069c64b2d58cb086ba3101cd045da2b953210
mariaach <cardenashenaomaria@gmail.com>
2023-04-24 11:03:55 -0500

    trabajando en la segunda parte del segundo modulo

--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -2,19 +2,19 @@ import { useState } from "react"
 import "./MiOrg.css"
 
 const MiOrg = (props) => {
-
+    //Estado - hooks
+    //useState
+    //const [nombreVariable,funcionActualiza] = useState(valorInicial)
     console.log(props)
-
-/*const [mostrar, actualizarMostrar] = useState(true)
-const manejarClick = (props) => {
-        console.log("Mostrar/Ocultar elemento", !mostrar)
-        actualizarMostrar(!mostrar)
-    }*/
-
+    // const [mostrar, actualizarMostrar] = useState(true)
+    // const manejarClick = () => {
+    //     console.log("Mostrar/Ocultar elemento", !mostrar)
+    //     actualizarMostrar(!mostrar)
+    // }
 
     return <section className="orgSection">
         <h3 className="title">Mi organización</h3>
-        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar}/>
+        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar} />
     </section>
 }
 
